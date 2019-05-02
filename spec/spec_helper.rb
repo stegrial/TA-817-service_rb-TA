890d6fec0c1be80f7d7860218fa9771fc426dd60
Grigoriy Stetsenko <stetsenko@softesis.com>
2019-05-02 11:27:42 +0300

    creating project

---
+++ spec/spec_helper.rb
@@ -0,0 +1,60 @@
+require 'bundler/setup'
+require 'ostruct'
+require 'selenium-webdriver'
+require 'rspec'
+require 'rspec-steps'
+require 'capybara/rspec'
+require 'true_automation/rspec'
+require 'true_automation/driver/capybara'
+
+def camelize(str)
+  str.split('_').map {|w| w.capitalize}.join
+end
+
+spec_dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(spec_dir)
+
+$data = {}
+Dir[File.join(spec_dir, 'fixtures/**/*.yml')].each {|f|
+  title = File.basename(f, '.yml')
+  $data[title] = OpenStruct.new(YAML::load(File.open(f)))
+}
+
+$data = OpenStruct.new($data)
+Dir[File.join(spec_dir, 'support/**/*.rb')].each {|f| require f}
+
+RSpec.configure do |config|
+  services = Selenium::WebDriver::Service.chrome(port: 3333, args: {
+      log_path: "chrome#{Time.now.to_i}.log",
+      verbose: true
+  })
+
+  # services = Selenium::WebDriver::Service.firefox(port: 3333, args: {
+  #     log: "trace"
+  # })
+  p services
+
+  Capybara.register_driver :true_automation_driver do |app|
+    TrueAutomation::Driver::Capybara.new(app, browser: :chrome, service: services)
+  end
+
+  # Capybara.register_driver :true_automation_driver do |app|
+  #   TrueAutomation::Driver::Capybara.new(app, browser: :firefox, service: services)
+  # end
+
+  Capybara.configure do |capybara|
+    capybara.run_server = false
+    capybara.default_max_wait_time = 5
+
+    capybara.default_driver = :true_automation_driver
+  end
+
+  config.include Capybara::DSL
+  config.include TrueAutomation::DSL
+
+  Dir[File.join(spec_dir, 'support/**/*.rb')].each {|f|
+    base = File.basename(f, '.rb')
+    klass = camelize(base)
+    config.include Module.const_get(klass)
+  }
+end
---
+++ spec/test_scenario/trueautomation_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature 'TrueAutomation.IO capybara example' do
+  # scenario 'Test example' do
+  #   visit 'https://trueautomation.io/'
+  #
+  #   find(:xpath, ta('trueautomationio:home:loginBtn', "//div[./span[text()='Login']]")).click
+  #
+  #   find(:css, ta('trueautomationio:signin:signupBtn', 'div.sign-up-container > a')).click
+  #
+  #   fill_in ta('trueautomationio:signup:email', 'email'), with: 'your@mail.com'
+  #   sleep 3
+  # end
+
+  scenario 'Test example' do
+    visit 'https://trueautomation.io/'
+
+    find(:xpath, "//div[./span[text()='Login']]").click
+
+    find(:css, 'div.sign-up-container > a').click
+
+    fill_in('email', with: 'your@mail.com')
+    sleep 3
+  end
+end
